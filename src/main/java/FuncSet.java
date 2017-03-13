de2b99cda595ad5d1f1dad790ef152a32666dd64
Ivan Ermolaev <ermolaevym@gmail.com>
2017-03-14 00:42:06 +0300

    init

---
+++ src/main/java/FuncSet.java
@@ -0,0 +1,53 @@
+import com.sun.tools.javac.util.Pair;
+
+import java.util.Arrays;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+/**
+ * Created by Ivan Ermolaev
+ * ermolaevym@gmail.com
+ */
+public class FuncSet {
+
+    private static final int BOUNDS = 1000;
+    public interface Set extends Function<Integer, Boolean> {}
+
+    public static Set singletonSet(Integer value) { return x -> value == x; }
+    public static Boolean contains(Set s, Integer value) { return s.apply(value); }
+    public static Set union(Set s1, Set s2) { return x -> s1.apply(x) || s2.apply(x); }
+    public static Set intersect(Set s1, Set s2) { return x -> s1.apply(x) && s2.apply(x); }
+    public static Set diff(Set s1, Set s2) { return x -> s1.apply(x) && !s2.apply(x); }
+    public static Set filter(Set s1, Function<Integer, Boolean> filter) { return x -> s1.apply(x) && filter.apply(x); }
+
+    public static Set setFrom(Integer... values) {
+
+        BiFunction<BiFunction, Pair<Set, Integer[]>, Set> funcIteration = (BiFunction func, Pair<Set, Integer[]> pair) -> {
+            if(pair.snd == null) return pair.fst;
+            else return (Set)func.apply(
+                    func,
+                    new Pair<> (
+                            pair.fst == null ?
+                                    singletonSet(getLast(pair.snd))
+                                    :
+                                    union(pair.fst, singletonSet(getLast(pair.snd))),
+                            getShorter(pair.snd)
+                    )
+            );
+        };
+        return funcIteration.apply(funcIteration, new Pair<>(null, values));
+    }
+
+    public static Integer[] getShorter(Integer[] values) {
+        if(values.length - 1 == 0) {
+            return null;
+        }
+        else {
+            return Arrays.copyOfRange(values, 0, values.length-1);
+        }
+    }
+
+    public static Integer getLast(Integer[] values) {
+        return values[values.length-1];
+    }
+}
---
+++ src/test/java/FuncSetTest.java
@@ -0,0 +1,67 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Ivan Ermolaev
+ * ermolaevym@gmail.com
+ */
+public class FuncSetTest {
+
+    @Test
+    public void test_singletonSet() {
+        FuncSet.Set singletonSet = FuncSet.singletonSet(2);
+        assertTrue(FuncSet.contains(singletonSet, 2));
+    }
+
+    @Test
+    public void test_setFrom() {
+        FuncSet.Set setObject = FuncSet.setFrom(1, 2, 3, 4);
+        assertTrue(FuncSet.contains(setObject, 1));
+        assertTrue(FuncSet.contains(setObject, 2));
+        assertTrue(FuncSet.contains(setObject, 3));
+        assertTrue(FuncSet.contains(setObject, 4));
+    }
+
+    @Test
+    public void test_union() {
+        FuncSet.Set set1 = FuncSet.setFrom(1, 2);
+        FuncSet.Set set2 = FuncSet.setFrom(3, 4);
+        FuncSet.Set unionSet = FuncSet.union(set1, set2);
+
+        assertTrue(FuncSet.contains(unionSet, 1));
+        assertTrue(FuncSet.contains(unionSet, 2));
+        assertTrue(FuncSet.contains(unionSet, 3));
+        assertTrue(FuncSet.contains(unionSet, 4));
+    }
+
+    @Test
+    public void test_intersect() {
+        FuncSet.Set set1 = FuncSet.setFrom(1, 2, 3, 4);
+        FuncSet.Set set2 = FuncSet.setFrom(3, 4, 5, 6);
+        FuncSet.Set intersectSet = FuncSet.intersect(set1, set2);
+
+        assertTrue(FuncSet.contains(intersectSet, 3));
+        assertTrue(FuncSet.contains(intersectSet, 4));
+    }
+
+    @Test
+    public void test_diff() {
+        FuncSet.Set set1 = FuncSet.setFrom(1, 2, 3, 4);
+        FuncSet.Set set2 = FuncSet.setFrom(3, 4, 5, 6);
+        FuncSet.Set diffSet = FuncSet.diff(set1, set2);
+
+        assertTrue(FuncSet.contains(diffSet, 1));
+        assertTrue(FuncSet.contains(diffSet, 2));
+    }
+
+    @Test
+    public void test_filter() {
+        FuncSet.Set numSet = FuncSet.setFrom(1, 2, 3, 4, 5);
+        FuncSet.Set filteredSet = FuncSet.filter(numSet, x -> x % 2 != 0);
+
+        assertTrue(FuncSet.contains(filteredSet, 1));
+        assertTrue(FuncSet.contains(filteredSet, 3));
+        assertTrue(FuncSet.contains(filteredSet, 5));
+    }
+}
